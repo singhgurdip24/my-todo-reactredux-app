96e11f4cfbea75a943ad1b56fe6a6e3f2a1b8b1e
Gurdip Singh <singh.gurdip24@gmail.com>
2020-03-24 22:18:02 -0400

    added some components

---
+++ src/components/TodoList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Todo from './Todo'
+import todos from '../reducers/todos'
+
+const TodoList = ({todo, toggleTodo}) => {
+    <ul>
+        {todos.map( todo => (
+            <Todo key={todo.id} {...todo} onClick={()=>toggleTodo(todo.id)} />
+        ))}
+    </ul>
+}
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            completed: PropTypes.bool.isRequired
+        })
+    )
+}
+
+export default TodoList
